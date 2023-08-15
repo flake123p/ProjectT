df645d1c1c8922f351e535c2ff3c2418fe2b71ab
flake123p <flake123p@gmail.com>
2023-08-15 23:35:46 +0800

    update

--- cpp/leetcode/include/Vec.hpp
+++ cpp/leetcode/include/Vec.hpp
@@ -1,1 +1,44 @@
 
+template <typename t>
+auto VecMake(int shape, t *data) {
+    vector<t> ret;
+    for (int i = 0; i < shape; i++) {
+        ret.push_back(*data);
+        data++;
+    }
+    return std::move(ret);
+};
+
+template <typename t>
+auto VecMake(int shape0, int shape1, t *data) {
+    vector<vector<t>> ret0;
+    for (int i = 0; i < shape0; i++) {
+        vector<t> ret1;
+        for (int j = 0; j < shape1; j++) {
+            ret1.push_back(*data);
+            data++;
+        }
+        ret0.push_back(std::move(ret1));
+    }
+    return std::move(ret0);
+};
+
+template <typename t>
+void VecDump(vector<t> &m) {
+    printf("%s():\n", __func__);
+    for (int i = 0; i < m.size(); i++) {
+        cout << m[i] << "\t";
+    }
+    printf("\n");
+};
+
+template <typename t>
+void VecDump(vector<vector<t>> &m) {
+    printf("%s():\n", __func__);
+    for (int i = 0; i < m.size(); i++) {
+        for (int j = 0; j < m[i].size(); j++) {
+            cout << m[i][j] << "\t";
+        }
+        printf("\n");
+    }
+};
