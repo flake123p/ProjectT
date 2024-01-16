48296868ea5a90a0c5ab3ab27d7e1ca51551f069
flake.yeh <flake.yeh@nui-ai.com>
2024-01-16 09:14:54 +0800

    update

--- cpp/projects/p01/app/app_test/main.cpp
+++ cpp/projects/p01/app/app_test/main.cpp
@@ -1,7 +1,51 @@
 #include "mnist_for_c.h"
+#include "_float.h"
+
+void Float_Test() 
+{
+    Fp32Cell f32;
+    float x = 0.33;
+
+    f32.f = x;
+    f32.dump_hex();
+
+    Fp64Cell f64;
+    double y = 33.0f;
+
+    f64.f = y;
+    f64.dump_hex();
+
+    printf("f32\n");
+    f32.dump_hex_fields();
+    f32.dump_fields();
+    f32.dump_fields_FloatFrac();
+    printf("f64\n");
+    f64.dump_hex_fields();
+    f64.dump_fields();
+    f64.dump_fields_FloatFrac();
+
+    printf("f32 to f64 manual\n");
+    f64.sign = f32.sign;
+    f64.expo = ((int)f32.expo) - f32.expoCmpl + f64.expoCmpl;
+    f64.frac = ((uint64_t)f32.frac)<<29;
+    f64.dump_fields();
+    f64.dump_fields_FloatFrac();
+
+    printf("f32 to f64 auto\n");
+    f64.f = 0; //clear
+    f64.f = f32.f;
+    f64.dump_fields();
+    f64.dump_fields_FloatFrac();
+    
+
+}
+
 
 int main()
 {
-    mnist_for_c_example();
+    //mnist_for_c_example();
+
+    dump_float_limits();
+    Float_Test();
     return 0;
 }
---
+++ cpp/projects/p01/mod/basic/_float.h
@@ -0,0 +1,123 @@
+#pragma once
+
+#include "basic.h"
+#include <math.h> // pow()
+
+/*
+    The limits in float.h.
+
+    Ref: https://www.tutorialspoint.com/c_standard_library/float_h.htm
+
+    FLT_MAX, FLT_MIN, ...
+*/
+#include "float.h"
+inline void dump_float_limits () {
+    printf("The maximum value of float = %.10e\n", FLT_MAX);
+    printf("The minimum value of float = %.10e\n", FLT_MIN);
+    printf("The number of digits in the number = %.d\n", FLT_MANT_DIG);
+    printf("The maximum value of double = %.10e\n", DBL_MAX);
+    printf("The minimum value of double = %.10e\n", DBL_MIN);
+    printf("The number of digits in the number = %.d\n", DBL_MANT_DIG);
+};
+
+/*
+    IEEE754 Format:
+
+    Ref: 
+        https://en.wikipedia.org/wiki/IEEE_754
+        https://moocaholic.medium.com/fp64-fp32-fp16-bfloat16-tf32-and-other-members-of-the-zoo-a1ca7897d407
+
+                Total Bits  Sign Bit    Exp Bits        Fraction Bits
+    BF16        16          1           8               7
+    TF32        19          1           8               10
+    half        16          1           5               10
+    float       32          1           8               23
+    double      64          1           11              52
+    
+*/
+
+template<typename TypeF, typename TypeU, int ExpoBits, int FracBits>
+struct FloatCell {
+    union {
+        TypeF f;
+        TypeU u;
+        struct {
+            TypeU frac : FracBits;
+            TypeU expo : ExpoBits;
+            TypeU sign : 1;
+        };
+    };
+    const int expoCmpl = (1 << (ExpoBits-1)) - 1;
+
+    void dump_hex(int newLine = 1) {
+        printf("[%-25s] ", __func__);
+        if (sizeof(TypeU) == 1)
+            printf("0x%02X", (unsigned int)u);
+        else if (sizeof(TypeU) == 2)
+            printf("0x%04X", (unsigned int)u);
+        else if (sizeof(TypeU) == 4)
+            printf("0x%08X", (unsigned int)u);
+        else if (sizeof(TypeU) == 8)
+            printf("0x%016lX", (long unsigned int)u);
+        else {
+            BASIC_ASSERT(0);
+        }
+        
+        if (newLine)
+            printf("\n");
+    };
+
+    void dump_hex_fields(int newLine = 1) {
+        printf("[%-25s] ", __func__);
+        printf("0x%lX, ", (long unsigned int)sign);
+        printf("0x%lX, ", (long unsigned int)expo);
+        printf("0x%lX, ", (long unsigned int)frac);
+        
+        if (newLine)
+            printf("\n");
+    };
+
+    void dump_fields(int newLine = 1) {
+        int expoReal = ((int)expo) - expoCmpl;
+        printf("[%-25s] ", __func__);
+        printf("%d, ", (int)sign);
+        printf("%d, ", expoReal);
+        printf("0x%lX, ", (long unsigned int)frac);
+        
+        if (newLine)
+            printf("\n");
+    };
+
+    void dump_fields_FloatFrac(int newLine = 1) {
+        int expoReal = ((int)expo) - expoCmpl;
+        printf("[%-25s] ", __func__);
+        printf("%d, ", (int)sign);
+        printf("%d, ", expoReal);
+        //printf("0x%lX, ", (long unsigned int)frac);
+        {
+            double accu = 1.0;
+            double curDbl = 1.0/2.0;
+            TypeU curU = (TypeU)frac;
+            TypeU MSB = (TypeU)1 << (FracBits - 1);
+
+            for (int i = 0; i<FracBits; i++) {
+                if (MSB & curU) {
+                    accu += curDbl;
+                }
+                curDbl /= 2.0;
+                curU <<= 1;
+            }
+            double exp = pow(2, expoReal);
+            printf("%f * %f = %f", accu, exp, accu*exp);
+        }
+        
+        if (newLine)
+            printf("\n");
+    };
+};
+
+using Bf16Cell = FloatCell<uint16_t, uint16_t, 8, 7>;
+using Fp16Cell = FloatCell<uint16_t, uint16_t, 5, 10>;
+using Fp32Cell = FloatCell<float, uint32_t, 8, 23>;
+using Tf32Cell = FloatCell<float, uint32_t, 8, 10>;  // need test the bit-fields
+using Fp64Cell = FloatCell<double, uint64_t, 11, 52>;
